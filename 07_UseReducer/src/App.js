dd8f478aa8a8bb2bfd8d16d58c5ed05d4e622d64
ajeyanadig <ajeyanadig@gmail.com>
2024-03-23 00:10:26 +0530

    Layout and Reducer set

--- 07_UseReducer/src/App.js
+++ 07_UseReducer/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import Header from "./Components/Header";
+import Main from "./Main";
+import Loader from "./Components/Loader";
+import Error from "./Components/Error";
+import StartScreen from "./StartScreen";
+//loading,error,ready,active,finished
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, status: "ready", questions: action.payload };
 
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    case "loading":
+      return { ...state, status: "loading" };
+
+    default:
+      throw new Error("Something is wrong in the reducer");
+  }
+}
 function App() {
+  let [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        dispatch({ type: "dataReceived", payload: data });
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch({ type: "dataFailed" });
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen questions={questions} />}
+      </Main>
     </div>
   );
 }
